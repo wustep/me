0820d920a0665b2e4642ae0ec3eecf901bf478ec
Stephen Wu <wustep@makenotion.com>
2022-11-21 23:47:35 -0800

    Update footer with link to main website

--- components/wustep/PostFooter.tsx
+++ components/wustep/PostFooter.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react'
 
+import { site } from '@/lib/config'
+
 import ApplauseButton from './ApplauseButton'
 
-export const PostFooter: React.FC = () => {
-  return (
-    <div className='wustep-post-footer'>
-      <div className='wustep-post-footer-left'>
+export const PostFooter: React.FC = () => (
+  <div className='wustep-post-footer'>
+    <div className='wustep-post-footer-left'>
+      <a href={`https://${site.domain}`}>
         <img src='/wustep.png' alt='Photo of Stephen Wu'></img>
-      </div>
-      <div className='wustep-post-footer-content'>
-        <div className='wustep-post-footer-content-author'>Stephen Wu</div>
-        🚀 Building software in San Francisco, CA
-      </div>
-      <div className='wustep-post-footer-right'>
-        <ApplauseButton />
-      </div>
+      </a>
     </div>
-  )
-}
+    <div className='wustep-post-footer-content'>
+      <div className='wustep-post-footer-content-author'>Stephen Wu</div>
+      🚀 Building software in San Francisco, CA
+    </div>
+    <div className='wustep-post-footer-right'>
+      <ApplauseButton />
+    </div>
+  </div>
+)
