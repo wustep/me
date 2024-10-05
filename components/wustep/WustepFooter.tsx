2e57177e557577f511cb0bc01440e70c55ef4d80
Stephen Wu <wujstephen@gmail.com>
2024-10-05 14:43:02 -0700

    Remove applause button

--- components/wustep/WustepFooter.tsx
+++ components/wustep/WustepFooter.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 import { site } from '@/lib/config'
 
-import { ApplauseButton } from './ApplauseButton'
+// import { ApplauseButton } from './ApplauseButton'
 
 /**
  * @wustep: Added my own personal footer
@@ -19,9 +19,12 @@ export const WustepFooter = () => {
         <div className='wustep-post-footer-content-author'>Stephen Wu</div>
         🚀 Building software in San Francisco, CA
       </div>
+      {/* Sadly applause is no longer working w/o self-hosting. See https://github.com/ColinEberhardt/applause-button/issues/101
+          Disabling for now until working again.
       <div className='wustep-post-footer-right'>
         <ApplauseButton />
-      </div>
+      </div> 
+      */}
     </div>
   )
 }
