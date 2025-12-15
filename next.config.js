32c6343e026587e4af5192cbde528e9822988ceb
Stephen Wu <wujstephen@gmail.com>
2025-12-15 02:36:52 -0800

    Upgrade npm & react

--- next.config.js
+++ next.config.js
@@ -8,8 +8,13 @@ const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
 
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
+  turbopack: {
+    root: dirname,
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "www.notion.so" },
@@ -62,7 +67,6 @@ export default withBundleAnalyzer({
     // Workaround for ensuring that `react` and `react-dom` resolve correctly
     // when using a locally-linked version of `react-notion-x`.
     // @see https://github.com/vercel/next.js/issues/50391
-    const dirname = path.dirname(fileURLToPath(import.meta.url));
     config.resolve.alias.react = path.resolve(dirname, "node_modules/react");
     config.resolve.alias["react-dom"] = path.resolve(
       dirname,
