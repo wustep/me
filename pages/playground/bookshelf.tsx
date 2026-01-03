b631838e69d7ffcc78761d726a7cf1ebfb76f6da
Stephen Wu <wujstephen@gmail.com>
2026-01-02 20:10:18 -0800

    Bookshelf propagate darkmode

--- pages/playground/bookshelf.tsx
+++ pages/playground/bookshelf.tsx
@@ -1,20 +1,33 @@
+import { useEffect, useState } from 'react'
+
 import { PlaygroundLayout } from '@/components/wustep/PlaygroundLayout'
+import { useDarkMode } from '@/lib/use-dark-mode'
 
 export default function PlaygroundBookshelfPage() {
+  const [hasMounted, setHasMounted] = useState(false)
+  const { isDarkMode } = useDarkMode()
+  const mode = isDarkMode ? 'dark' : 'light'
+
+  useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
   return (
     <PlaygroundLayout
       title='Bookshelf'
       breadcrumbs={[{ label: 'Bookshelf' }]}
       fullFrame
     >
-      <iframe
-        src='https://wustep-bookshelf.vercel.app/'
-        title='Bookshelf iframe'
-        className='flex-1 w-full border-0'
-        loading='lazy'
-        allow='fullscreen *'
-        allowFullScreen
-      />
+      {hasMounted && (
+        <iframe
+          src={`https://wustep-bookshelf.vercel.app/?mode=${mode}`}
+          title='Bookshelf iframe'
+          className='flex-1 w-full border-0'
+          loading='lazy'
+          allow='fullscreen *'
+          allowFullScreen
+        />
+      )}
     </PlaygroundLayout>
   )
 }
